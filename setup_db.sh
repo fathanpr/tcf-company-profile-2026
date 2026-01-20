18cc9830e70ac100bfa4b144879f7f70fcf4c28a
fathan <maxtyo.17@gmail.com>
2026-01-21 06:33:49 +0700

    Update delete all trash

--- setup_db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Reset PostgreSQL user password
-echo "nevergiveup" | sudo -S -u postgres psql -c "ALTER USER tcf_admin WITH PASSWORD 'tcf_secure_pass';"
-# Grant public schema permissions
-echo "nevergiveup" | sudo -S -u postgres psql -d tcf_company_profile -c "GRANT ALL ON SCHEMA public TO tcf_admin;"
-# Run migrations
-cd /var/www/tcf-company-profile
-php artisan migrate --force
